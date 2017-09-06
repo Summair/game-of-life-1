ee04f29f5d26b7685edadaa1df64c31fdda5bade
kmlchauhan <kmlchauhan@gmail.com>
2017-09-06 20:45:57 +0530

    Adding export commands

--- mvnBuild.sh
+++ mvnBuild.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+JAVA_HOME=/opt/java/jdk1.8.0_112
+PATH=$PATH:$JAVA_HOME/bin
+MAVEN_HOME=/opt/apache-maven-3.5.0
+PATH=$PATH:$MAVEN_HOME/bin
+export JAVA_HOME
+export MAVEN_HOME
+export PATH
+
 mvn clean package
